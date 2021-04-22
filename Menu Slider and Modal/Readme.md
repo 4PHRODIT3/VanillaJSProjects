36766694143f9a710be585b3a2ce41bd632bb6ca
Htet-Myat-Linn <b8_htetmyatlinn@uit.edu.mm>
2021-04-22 16:53:03 +0630

    Update Readme.md

---
+++ Menu Slider and Modal/Readme.md
@@ -0,0 +1,15 @@
+# This project contains mostly css.
+
+# First create postition relative header and fill some contents
+
+# Then create body for some paragraphs
+
+# Nav-slide is created outside the body tag by using flex position and translateX(-100%)
+
+# Add some JavaScript to make simple show navigation button by making toggle to translateX(your nav-slide width)
+
+# Sign Up modal can be done easily with position fixed, width 100% and height 100%
+
+# Then Use z-index for priority
+
+# Finally Make Responsive Boo yaaa!
--- Menu Slider and Modal/app.js
+++ Menu Slider and Modal/app.js
@@ -1,15 +1,35 @@
 const nav_btn = document.getElementById("nav-toggle");
 const body = document.body;
 const nav_bar = document.querySelector(".nav-bar");
+const signup = document.getElementById("sign-up");
+const modal = document.querySelector(".modal");
+const modal_close = document.getElementById("modal-close");
 
 const closeNavbar = e => {
-    if(body.classList.contains("show-nav") && e.target !== nav_btn && !nav_btn.contains(e.target) && e.target !== nav_bar && !nav_bar.contains(e.target)){
-        body.classList.remove("show-nav");
-    }
-        
+    if(body.classList.contains("show-nav")){
+        if(e.target !== nav_btn && !nav_btn.contains(e.target)){
+            if(e.target !== nav_bar && !nav_bar.contains(e.target)){
+                body.classList.remove("show-nav");
+            }
+        }
+    }  
 }
 
 nav_btn.addEventListener("click", _ => {
     body.classList.toggle("show-nav");
     body.addEventListener("click", closeNavbar);
+})
+
+signup.addEventListener("click", _ => {
+    modal.classList.add("show-modal");
+})
+
+modal_close.addEventListener("click", _ => {
+    modal.classList.remove("show-modal");
+})
+
+window.addEventListener("click", e => {
+    if(e.target == modal){
+        modal.classList.remove("show-modal");
+    }
 })
