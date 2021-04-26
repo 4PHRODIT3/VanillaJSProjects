3397949470e072c3452596b2de341e27d0374219
Htet-Myat-Linn <b8_htetmyatlinn@uit.edu.mm>
2021-04-26 18:18:00 +0630

    Added Meal Finder UI

---
+++ Meal Finder/app.js
@@ -0,0 +1,5 @@
+const search_input = document.getElementById("search-text");
+const search_btn = document.getElementById("search");
+const random_btn = document.getElementById("random");
+const images_section = document.querySelector(".images-section");
+const detail_container = document.querySelector(".detail-container");
