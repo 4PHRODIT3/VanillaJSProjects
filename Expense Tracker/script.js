4fd924aa1eb91ca1dc060c14b8a1634bdf6604d4
Htet-Myat-Linn <b8_htetmyatlinn@uit.edu.mm>
2021-05-04 03:19:02 +0630

    Fixed Safari Errors for Expnese Tracker App

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -1,6 +1,7 @@
 const info = document.getElementById("info");
 const message_box = document.querySelector(".message-box");
 
+
 info.addEventListener("click", _ => {
     
     message_box.style.display = "inline-block";
@@ -9,3 +10,4 @@ info.addEventListener("click", _ => {
     },3000);
 
 })
+
