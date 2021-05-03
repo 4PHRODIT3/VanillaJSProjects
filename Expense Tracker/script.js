64858b8370d2aa0d6a571f183d39b2c80a37aa7b
Htet-Myat-Linn <b8_htetmyatlinn@uit.edu.mm>
2021-05-04 02:48:51 +0630

    Neumorphic UI Expense Tracker Added

---
+++ Expense Tracker/script.js
@@ -0,0 +1,11 @@
+const info = document.getElementById("info");
+const message_box = document.querySelector(".message-box");
+
+info.addEventListener("click", _ => {
+    
+    message_box.style.display = "inline-block";
+    setTimeout( _ => {
+        message_box.style.display = "none";
+    },3000);
+
+})
